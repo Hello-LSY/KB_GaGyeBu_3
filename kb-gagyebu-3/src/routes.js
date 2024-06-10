266842781b72f65df368bae70d88dc989e49da4b
Hello-LSY <leesy9520@naver.com>
2024-06-10 14:55:38 +0900

    [이신영] add registTransactionForm

--- kb-gagyebu-3/src/routes.js
+++ kb-gagyebu-3/src/routes.js
@@ -1,6 +1,7 @@
 import DashboardView from './views/DashboardView.vue'
 import AnalyticsView from './views/AnalyticsView.vue'
 import TransactionView from './views/TransactionView.vue'
+import RegistTransactionView from './views/RegistTransactionView.vue'
 import SettingsView from './views/SettingsView.vue'
 import AccountView from './views/AccountView.vue'
 
@@ -8,6 +9,7 @@ const routes = [
   { path: '/', component: DashboardView },
   { path: '/analytics', component: AnalyticsView },
   { path: '/transaction', component: TransactionView },
+  { path: '/transaction/regist', component: RegistTransactionView},
   { path: '/settings', component: SettingsView },
   { path: '/account', component: AccountView },
 ]
--- kb-gagyebu-3/src/views/TransactionView.vue
+++ kb-gagyebu-3/src/views/TransactionView.vue
@@ -1,14 +1,13 @@
 <template>
-    <div>
-      <h1>Transaction</h1>
-      <!-- 거래 내용 -->
-    </div>
-  </template>
-  
-  <script setup>
-  </script>
-  
-  <style scoped>
-  /* 스타일 정의 */
-  </style>
-  
+  <div>
+    <h1>Transaction</h1>
+    <!-- 거래 내용 -->
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+/* 스타일 정의 */
+</style>
