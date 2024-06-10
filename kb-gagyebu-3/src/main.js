468ca6f1750e899163b4ab4e86383728bda687c9
taekyunkim <dkssudgo112@naver.com>
2024-06-10 15:55:26 +0900

    [김태균] fix margin bug

--- kb-gagyebu-3/src/main.js
+++ kb-gagyebu-3/src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import routes from './routes'  // 경로가 올바른지 확인
 
 const app = createApp(App)
