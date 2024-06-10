6311a86da9769d35cd326ac761318704dafcb0a7
taekyunkim <dkssudgo112@naver.com>
2024-06-10 11:16:56 +0900

    [김태균] router 추가

---
+++ kb-gagyebu-3/src/routes.js
@@ -0,0 +1,16 @@
+// src/routes.js
+import Home from './views/Home.vue'
+import About from './views/About.vue'
+
+const routes = [
+  {
+    path: '/',
+    component: Home,
+  },
+  {
+    path: '/about',
+    component: About,
+  },
+]
+
+export default routes
