1f5aaebeabb0cce34e8f8f2f8b59247fddd3893e
taekyunkim <dkssudgo112@naver.com>
2024-06-10 11:16:56 +0900

    [김태균] router 추가

---
+++ kb-gagyebu-3/src/views/About.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <h1>About</h1>
+      <p>This is the about page.</p>
+      <router-link to="/">Go to Home</router-link>
+    </div>
+  </template>
+  
+  <script setup>
+  </script>
+  
+  <style>
+  /* 스타일을 여기에 추가합니다 */
+  </style>
+  
