a32ec0e7201927e7557a6d1d60114c1ee8140133
Hello-LSY <leesy9520@naver.com>
2024-06-11 13:29:43 +0900

    [이신영] refactor auth.js

--- kb-gagyebu-3/src/stores/auth.js
+++ kb-gagyebu-3/src/stores/auth.js
@@ -39,6 +39,19 @@ export const useAuthStore = defineStore('auth', {
           this.token = btoa(`${user.email}:${user.password}`); // Base64 암호화
           localStorage.setItem('token', this.token);
           localStorage.setItem('user', JSON.stringify(user)); // 사용자 정보도 저장
+
+          // settings 추가
+          const settingsResponse = await axios.get(`http://localhost:3000/settings?userId=${user.id}`);
+          if (settingsResponse.data.length === 0) {
+            //없는경우 추가(첫 생성)
+            await axios.post('http://localhost:3000/settings', {
+              userId: user.id,
+              notifications: true,
+              language: "kor",
+              theme: "light"
+            });
+          }
+
           alert('로그인 성공');
         } else {
           throw new Error('이메일 혹은 비밀번호가 일치하지 않습니다.');
